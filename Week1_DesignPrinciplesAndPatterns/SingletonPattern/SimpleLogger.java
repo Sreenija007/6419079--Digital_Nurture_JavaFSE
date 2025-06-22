dbf34308b52f3ec61c56c23f5b160ddd6f8c84a4
Sreenija Swetha <msreenija07@gmail.com>
2025-06-22 21:50:11 +0530

    SimpleLogger.java

--- Week1_DesignPrinciplesAndPatterns/SingletonPattern/SimpleLogger.java
+++ Week1_DesignPrinciplesAndPatterns/SingletonPattern/SimpleLogger.java
@@ -1,1 +1,18 @@
+public class SimpleLogger {
+    // Eagerly create the only instance
+    private static final SimpleLogger loggerInstance = new SimpleLogger();
+
+    // Private constructor so no one else can create this
+    private SimpleLogger() {
+        System.out.println("SimpleLogger is ready!");
+    }
+
+    // Public method to access the instance
+    public static SimpleLogger getLogger() {
+        return loggerInstance;
+    }
+    public void show(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
 
